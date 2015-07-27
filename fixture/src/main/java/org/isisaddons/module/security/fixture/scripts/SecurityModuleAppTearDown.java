8b4ada7be5734f3299a8f1b90985a76f587a2d1d
Jeroen van der Wal <jeroen@stromboli.it>
2015-07-27 18:19:13 +0200

    DN 4.1 upgrade (for ISIS-1153); syntax for schema and upgrade to dn enhancer plugin

--- fixture/src/main/java/org/isisaddons/module/security/fixture/scripts/SecurityModuleAppTearDown.java
+++ fixture/src/main/java/org/isisaddons/module/security/fixture/scripts/SecurityModuleAppTearDown.java
@@ -23,11 +23,11 @@ public class SecurityModuleAppTearDown extends FixtureScript {
 
     @Override
     protected void execute(ExecutionContext executionContext) {
-        isisJdoSupport.executeUpdate("delete from isissecurity.\"ApplicationPermission\"");
-        isisJdoSupport.executeUpdate("delete from isissecurity.\"ApplicationUserRoles\"");
-        isisJdoSupport.executeUpdate("delete from isissecurity.\"ApplicationRole\"");
-        isisJdoSupport.executeUpdate("delete from isissecurity.\"ApplicationUser\"");
-        isisJdoSupport.executeUpdate("delete from isissecurity.\"ApplicationTenancy\"");
+        isisJdoSupport.executeUpdate("delete from \"isissecurity\".\"ApplicationPermission\"");
+        isisJdoSupport.executeUpdate("delete from \"isissecurity\".\"ApplicationUserRoles\"");
+        isisJdoSupport.executeUpdate("delete from \"isissecurity\".\"ApplicationRole\"");
+        isisJdoSupport.executeUpdate("delete from \"isissecurity\".\"ApplicationUser\"");
+        isisJdoSupport.executeUpdate("delete from \"isissecurity\".\"ApplicationTenancy\"");
 
         isisJdoSupport.executeUpdate("delete from \"NonTenantedEntity\"");
         isisJdoSupport.executeUpdate("delete from \"TenantedEntity\"");
