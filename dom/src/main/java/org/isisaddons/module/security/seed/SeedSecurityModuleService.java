aee8da8c62be7ccab986a9fa8245d337362c5d3a
Dan Haywood <dan@haywood-associates.co.uk>
2015-09-20 10:59:49 +0100

    fixes #24 - add menuOrder to the seed service

--- dom/src/main/java/org/isisaddons/module/security/seed/SeedSecurityModuleService.java
+++ dom/src/main/java/org/isisaddons/module/security/seed/SeedSecurityModuleService.java
@@ -19,12 +19,16 @@ package org.isisaddons.module.security.seed;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.DomainServiceLayout;
 import org.apache.isis.applib.annotation.NatureOfService;
 import org.apache.isis.applib.annotation.Programmatic;
 import org.apache.isis.applib.fixturescripts.FixtureScripts;
 
 @DomainService(
         nature = NatureOfService.DOMAIN
+)
+@DomainServiceLayout(
+        menuOrder = "1000" // not visible, but determines the order initialized
 )
 public class SeedSecurityModuleService {
 
