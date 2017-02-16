889e54d07f0d94463597b65214f8f47c8b905919
Dan Haywood <dan@haywood-associates.co.uk>
2017-02-16 17:53:03 +0000

    updates bumpver_isis.sh to use mvn versions:set rather than sed

--- bumpver_isis.sh
+++ bumpver_isis.sh
@@ -5,18 +5,18 @@ if [ ! "$VERSION" ]; then
     exit 1
 fi
 
+MODULE_DIR=dom
+
 # edit parent pom.xml
 echo "editing parent pom.xml"
-cat pom.xml | sed "s/<isis.version>.*</<isis.version>$VERSION</" > pom.xml.$$.sed
-mv pom.xml.$$.sed pom.xml
+echo mvn versions:update-parent "-DparentVersion=[${VERSION}]"
+mvn versions:update-parent "-DparentVersion=[${VERSION}]"
 
-# edit dom's pom.xml
-echo "editing dom's pom.xml"
-pushd dom >/dev/null
-cat pom.xml | sed "s/<isis.version>.*</<isis.version>$VERSION</" > pom.xml.$$.sed
-mv pom.xml.$$.sed pom.xml
-popd >/dev/null
+# edit MODULE_DIR's pom.xml
+echo "editing $MODULE_DIR's pom.xml"
+echo mvn -pl $MODULE_DIR versions:update-parent "-DparentVersion=[${VERSION}]"
+mvn -pl $MODULE_DIR versions:update-parent "-DparentVersion=[${VERSION}]"
 
 echo "Committing changes"
-git commit -am "bumping isis.version to $VERSION"
+git commit -am "bumping incode-parent (isis) version to $VERSION"
 
